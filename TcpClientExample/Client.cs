c557369a5d0f38418290a948c5e41cbde27aa724
Tural Novruzov <turalinovruzov@gmail.com>
2020-07-29 02:33:15 +0400

    Add project files.

---
+++ TcpClientExample/Client.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpClientExample
+{
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            TcpClient tcpClient = new TcpClient();
+
+            var ip = IPAddress.Parse("192.168.1.105");
+            var port = 27001;
+            var ep = new IPEndPoint(ip, port);
+
+            BinaryWriter bw = null;
+
+            try
+            {
+                tcpClient.Connect(ep);
+
+                if (tcpClient.Connected)
+                {
+                    Console.WriteLine($"Connected Server: {tcpClient.Client.RemoteEndPoint}");
+
+                    while (true)
+                    {
+
+                        Console.WriteLine("Enter message: ");
+
+                        var msg = Console.ReadLine();
+
+                        //var bytes = Encoding.UTF8.GetBytes(msg);
+
+                        var networkStream = tcpClient.GetStream();
+
+                        bw = new BinaryWriter(networkStream);
+                        bw.Write(msg);
+
+                        //networkStream.Write(bytes, 0, bytes.Length);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
---
+++ TcpListenerExample/Server.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpListenerExample
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            var ip = IPAddress.Parse("192.168.1.105");
+            var port = 27001;  //(anyPort)
+            var ep = new IPEndPoint(ip, port);
+            var tcpListener = new TcpListener(ep);
+            tcpListener.Start();
+
+            BinaryReader br = null;
+            //BinaryWriter bw = null;
+
+            try
+            {
+
+
+                while (true)
+                {
+                    Console.WriteLine($"Listening on {tcpListener.LocalEndpoint}");
+
+                    TcpClient client = tcpListener.AcceptTcpClient();
+
+                    Console.WriteLine($"{client.Client.RemoteEndPoint} connected!");
+
+                    NetworkStream networkStream = client.GetStream();
+
+                    br = new BinaryReader(networkStream);
+
+                    while (true)
+                    {
+                        //example 1
+                        //var buffer = new byte[1024];
+                        //var lenght = networkStream.Read(buffer, 0, buffer.Length);
+                        //var msg = Encoding.UTF8.GetString(buffer, 0, lenght);
+
+
+
+                        //example with BinaryRead
+                        var msg = br.ReadString();
+                        Console.WriteLine($"Message: {msg}");
+                    }
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
